69b8963085b06e068b6b1012793e2e69b083f3ed
adi-uchiha <adi.cs.night@gmail.com>
2025-04-14 12:21:27 +0530

    Use the logo in dashboardnavbar

--- frontend/src/components/layout/DashboardNavbar.tsx
+++ frontend/src/components/layout/DashboardNavbar.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuth } from '@/contexts/AuthContext';
 import { CustomButton } from '../ui/custom-button';
-import { Button } from '../ui/button';
 
 const DashboardNavbar = () => {
   const { logout } = useAuth();
@@ -19,9 +18,22 @@ const DashboardNavbar = () => {
       <div className="container mx-auto px-4">
         <nav className="flex items-center justify-between">
           <div className="flex items-center space-x-8">
-            <div className="w-10 h-10 rounded-md bg-doodle-yellow border-2 border-black flex items-center justify-center">
-              <span className="text-black text-lg">D</span>
-            </div>
+            <Link 
+              to="/" 
+              className="flex items-center space-x-2 text-2xl font-bold font-space-grotesk"
+            >
+              <div className="w-10 h-10 rounded-md bg-doodle-yellow border-2 border-black flex items-center justify-center">
+                <div className='w-6 h-6'>
+
+                <img src="/DoodleDict.png" alt="" />
+                </div>
+              </div>
+              <span className="hidden sm:inline-block">
+                <div className='w-28 h-8'>
+                  <img src="/DoodleDict-Secondary.png" alt="" />
+                </div>
+              </span>
+            </Link>
             <Link to="/dashboard" className="font-bold hover:text-doodle-coral">
               Dashboard
             </Link>
